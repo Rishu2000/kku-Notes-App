61ebcec1c814261c6328751aef9c5487e662fbd1
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-05-04 14:05:00 +0530

    basic loging and logout functionality using buttons.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  
   return (
     <div className="App">
       <Header dark={true} className="Header">
-        Hello World!
+        Header
       </Header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <p>{user?`User Loged In  as ${user}`:"User Loged Out"}</p>
+            <div className="Button btn btn-primary"
+              onClick={(e) => {setUser("Rishav")}}
+            >LogIn</div>
+            <div className="Button btn btn-secondary ml-3"
+              onClick={(e) => {setUser(null)}}
+            >LogOut</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
