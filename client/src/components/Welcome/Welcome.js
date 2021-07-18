f881e386cbbb1a3df989da46f893f2ea64fd6309
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-18 17:01:21 +0530

    Converted function Component to Class Component of Welcome class and added actions to set and display the current node.

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import List from './List'
 
-const Welcome = ({user, handleLogout}) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <p className="mb-1">Welcome, {user}</p>
-                    <button className="btn btn-danger" onClick={handleLogout}>
-                        Logout
-                    </button>
+class Welcome extends Component {
+    state = {
+        Notes:["Note 1","Note 2","Note 3"],
+        CurrentNote:null,
+    }
+    setCurrentNote = CurrentNote => {
+        this.setState({CurrentNote})
+    }
+    render() {
+        const {user, handleLogout} = this.props;
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <p className="mb-1">Welcome, {user}</p>
+                        <button className="btn btn-danger" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-3">
+                        <List Notes={this.state.Notes} 
+                            CurrentNote = {this.state.CurrentNote}
+                            setCurrentNote={this.setCurrentNote}/>
+                    </div>
+                    <div className="col-9">Right Sidebar</div>
                 </div>
             </div>
-            <div className="row mt-3">
-                <div className="col-3">
-                    <List Notes={["Note 1","Note 2","Note 3"]}/>
-                </div>
-                <div className="col-9">Right Sidebar</div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 export default Welcome
