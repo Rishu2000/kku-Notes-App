4cf7b9943ba7b182de10336772933eef1c0acf5b
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-18 16:38:24 +0530

    Implemented left sidebar list.

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import List from './List'
 
 const Welcome = ({user, handleLogout}) => {
     return (
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <p>Welcome, {user}</p>
+                    <p className="mb-1">Welcome, {user}</p>
                     <button className="btn btn-danger" onClick={handleLogout}>
                         Logout
                     </button>
                 </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col-3">
+                    <List Notes={["Note 1","Note 2","Note 3"]}/>
+                </div>
+                <div className="col-9">Right Sidebar</div>
             </div>
         </div>
     )
