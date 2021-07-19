28b0ca304e282345b08db9f04230997d8686a7dc
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-19 17:35:47 +0530

    Added a Link to Home page.

--- client/src/components/Welcome/WelcomeHeader.js
+++ client/src/components/Welcome/WelcomeHeader.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const WelcomeHeader = ({user, handleLogout}) => {
     return (
         <div className="row">
             <div className="col-12">
                 <p className="mb-1">Welcome, {user}</p>
+                <Link to="/">
+                    <div className="btn btn-primary mr-2">Home</div>
+                </Link>
                 <button className="btn btn-danger" onClick={handleLogout}>
                     Logout
                 </button>
