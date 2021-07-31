aeaa9b29072d61249d6b7853ea4798c85d0794f0
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-31 12:52:00 +0530

    Reached to the boiler plate in the server-side.

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const session = require('express-session');
+const port = 3002;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(session({
+    secret: 'corona',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+app.get('/', (req, res) => {
+    res.json('Server has started.');
+})
+
+app.listen(port, () => {
+    console.log("server listening on port " + port);
+});
