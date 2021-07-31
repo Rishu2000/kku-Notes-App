c5ef84986130f38a41a6931e0e724005902d1c28
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-31 11:53:35 +0530

    Works this way as well (Without using render attribute.).

--- client/src/components/Welcome/Note.js
+++ client/src/components/Welcome/Note.js
@@ -7,7 +7,12 @@ const Note = ({ match, Notes}) => {
     return (
         <Switch>
             <Route path="/" exact={true} component={NoteWelcome}/>
-            <Route path="/:NoteID" render={rp => <NoteContent {...rp} Notes={Notes} />}/>
+            <Route path="/:NoteID"> 
+            {/*  render={rp => <NoteContent {...rp} Notes={Notes}  />}*/}
+                {rp => (
+                    <NoteContent {...rp} Notes={Notes}  />
+                )}
+            </Route>
         </Switch>
     )
 }
