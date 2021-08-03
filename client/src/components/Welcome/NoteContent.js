d0d08a1b3489f655a57f0783f324fd8b7b04821a
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-03 13:57:26 +0530

    Solved the error.

--- client/src/components/Welcome/NoteContent.js
+++ client/src/components/Welcome/NoteContent.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
-const NoteContent = ({match, Notes}) => {
-    const {Title, Desc} = Notes[+match.params.NoteID.replace("note-","")]
-    return (
-        <>
-        <pre className="border rounded p-4 bg-light">
-            <h3 className="mb-3">{Title}</h3>
-            <p>{Desc}</p>
-        </pre>
-        </>
-    )
+const NoteContent = ({match, NotesApi}) => {
+    if(!NotesApi) {
+        return (
+            <>
+            <pre className="border rounded p-4 bg-light">
+                <h3 className="mb-3">Please Wait</h3>
+                <p>Data are beening fetched.</p>
+            </pre>
+            </>
+        )
+    }
+    const Note = NotesApi[+match.params.NoteID.replace("note-","")]
+    if(!Note){
+        return (
+            <>
+            <pre className="border rounded p-4 bg-light">
+                <h3 className="mb-3">Note not found</h3>
+                <p>Note you are looking for is not available.</p>
+            </pre>
+            </>
+        )
+    }else{
+        const {Title, Desc} = Note;
+        return (
+            <>
+            <pre className="border rounded p-4 bg-light">
+                <h3 className="mb-3">{Title}</h3>
+                <p>{Desc}</p>
+            </pre>
+            </>
+        )
+    }
 }
 
 export default NoteContent
